28a53583744b51060b5c77a688a00464a91c70ab
Lóránt Pintér <lorant.pinter@gmail.com>
2024-08-07 11:59:46 +0200

    Look up MQTT from mDNS

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,13 @@ mod network;
 use esp_idf_svc::hal::task;
 use esp_idf_svc::sntp;
 use esp_idf_svc::timer::EspTaskTimerService;
-use esp_idf_svc::{eventloop::EspSystemEventLoop, nvs::EspDefaultNvsPartition};
+use esp_idf_svc::{
+    eventloop::EspSystemEventLoop,
+    mdns::{EspMdns, Interface, Protocol, QueryResult},
+    nvs::EspDefaultNvsPartition,
+};
 use std::future::pending;
+use std::time::Duration;
 
 fn main() -> anyhow::Result<()> {
     // It is necessary to call this function once. Otherwise some patches to the runtime
@@ -32,13 +37,36 @@ fn main() -> anyhow::Result<()> {
     let ip_info = wifi.sta_netif().get_ip_info()?;
     log::info!("WiFi DHCP info: {:?}", ip_info);
 
+    let mdns = EspMdns::take()?;
+    let mut results = [QueryResult {
+        instance_name: None,
+        hostname: None,
+        port: 0,
+        txt: Vec::new(),
+        addr: Vec::new(),
+        interface: Interface::STA,
+        ip_protocol: Protocol::V4,
+    }];
+
     let _sntp = sntp::EspSntp::new_with_callback(&Default::default(), |duration| {
         log::info!("SNTP time: {:?}", duration);
     })?;
     log::info!("Current time: {:?}", std::time::SystemTime::now());
 
-    let (mut mqtt, _) = network::connect_mqtt("mqtt://bumblebee.local", "ugly-duckling-rs-test")?;
-    let payload = "Hello, World!".as_bytes();
+    mdns.query_ptr(
+        "_mqtt",
+        "_tcp",
+        Duration::from_secs(5),
+        1,
+        &mut results,
+    )?;
+    log::info!("MDNS query result: {:?}", results);
+    let result = results[0].clone();
+    let mqtt_host = result.hostname.unwrap();
+    let mqtt_port = result.port;
+
+    let (mut mqtt, _) = network::connect_mqtt(&format!("mqtt://{}.local:{}", mqtt_host, mqtt_port), "ugly-duckling-rs-test")?;
+    let payload = "Hello mDNS!".as_bytes();
     task::block_on(mqtt.publish(
         "test",
         esp_idf_svc::mqtt::client::QoS::AtLeastOnce,
